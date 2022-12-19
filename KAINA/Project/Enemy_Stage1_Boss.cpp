86f16857e7d4063954d581c6126cb08926b80e96
Pureria <117608289+Pureria@users.noreply.github.com>
2022-12-19 14:48:42 +0900

    Update Enemy_Stage1_Boss.cpp
    斬撃攻撃の当たり判定調整

--- KAINA/Project/Enemy_Stage1_Boss.cpp
+++ KAINA/Project/Enemy_Stage1_Boss.cpp
@@ -320,12 +320,12 @@ void CEnemy_Stage1_Boss::Update() {
 			if (m_bReverse)
 			{
 				//m_MoveX = -ENEMY_ATTACKSLASH_MOVE;
-				m_AttakSlashRect = CRectangle(m_PosX - ENEMY_ATTACKSLASH_WIDTH, m_PosY, m_PosX, m_PosY + m_SrcRect.GetHeight());
+				m_AttakSlashRect = CRectangle(m_PosX - ENEMY_ATTACKSLASH_WIDTH + ENEMY_RECT_WIDTH_DECREASE, m_PosY, m_PosX + ENEMY_RECT_WIDTH_DECREASE, m_PosY + m_SrcRect.GetHeight());
 			}
 			else
 			{
 				//m_MoveX = ENEMY_ATTACKSLASH_MOVE;
-				m_AttakSlashRect = CRectangle(m_PosX + m_SrcRect.GetWidth(), m_PosY, m_PosX + m_SrcRect.GetWidth() + ENEMY_ATTACKSLASH_WIDTH, m_PosY + m_SrcRect.GetHeight());
+				m_AttakSlashRect = CRectangle(m_PosX + m_SrcRect.GetWidth() - ENEMY_RECT_WIDTH_DECREASE, m_PosY, m_PosX + m_SrcRect.GetWidth() + ENEMY_ATTACKSLASH_WIDTH - ENEMY_RECT_WIDTH_DECREASE, m_PosY + m_SrcRect.GetHeight());
 			}
 			m_AttackSlash = true;
 		}
